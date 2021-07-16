d9c799a6b475e9a3ee43a1a534f27ecf452f349f
Atharva-Gunaware <73386994+Atharva-Gunaware@users.noreply.github.com>
2021-07-16 11:26:09 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDjKDeW0TnEWQx7nHCGNT1jXU972ZC0oMU",
+    authDomain: "letschatwebapp-cf15a.firebaseapp.com",
+    projectId: "letschatwebapp-cf15a",
+    storageBucket: "letschatwebapp-cf15a.appspot.com",
+    messagingSenderId: "631247834042",
+    appId: "1:631247834042:web:9e5fc636b0a224aacf6b83"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
